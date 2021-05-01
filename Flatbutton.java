53c5d3941fa654570c8c8afe7631f5aa9e32c427
Archi991-dot <61641517+Archi991-dot@users.noreply.github.com>
2021-05-01 12:49:18 -0400

    FlatButton.java

--- Flatbutton.java
+++ Flatbutton.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.GUI;
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,8 +7,8 @@ import java.awt.*;
  * Project Name: Master 2D Calculator
  * Author: ARCHISMAN NATH on 5/1/2021 in 12:05 PM
  */
-public class Flatbutton extends JButton {
-    public Flatbutton(){
+class FlatButton extends JButton {
+    public FlatButton(){
         this.setBounds(50, 100, 95, 30);
         this.setBorderPainted(true);
         this.setFocusPainted(false);
