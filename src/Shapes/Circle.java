106f19788294ba4fca8da148030b832e0af4fd71
Archi991-dot <61641517+Archi991-dot@users.noreply.github.com>
2021-05-04 08:43:12 -0400

    added constructor

--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -1,4 +1,19 @@
-package src.Shapes;
+package com.Shapes;
 
-public class Circle {
+import com.Functionallity.UsefulMethods;
+
+/**
+ * Automatic Generation Project - Author
+ * Project Name: GeoMath-2D~3D
+ * Author: ARCHISMAN NATH on 5/3/2021 in 10:54 PM
+ */
+public class Circle extends UsefulMethods {
+
+    public Circle(double radius) {
+        UsefulMethods.circleArea(radius);
+        UsefulMethods.circleCircumference(radius);
+        UsefulMethods.calculatePi();
+        UsefulMethods.radiusFromArea(circleArea(radius));
+        UsefulMethods.radiusFromCircumference(circleCircumference(radius));
+    }
 }
