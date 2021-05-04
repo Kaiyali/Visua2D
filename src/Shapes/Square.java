dfda93dc06efc7c75f90b2e30295578e9ad49f1e
Archi991-dot <61641517+Archi991-dot@users.noreply.github.com>
2021-05-04 09:45:38 -0400

    Update Square.java

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -2,7 +2,7 @@ package com.Shapes;
 
 import com.Functionallity.UsefulMethods;
 
-public class Square extends UsefulMethods {
+public class Square {
 
     public Square(double length, double width, double side) {
         UsefulMethods.squareArea(length, width);
