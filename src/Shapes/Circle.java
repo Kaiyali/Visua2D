3988dfe25fcdb620e3ca73c64c874f60539a7795
Archi991-dot <61641517+Archi991-dot@users.noreply.github.com>
2021-05-04 09:46:27 -0400

    Update Circle.java

--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -7,7 +7,7 @@ import com.Functionallity.UsefulMethods;
  * Project Name: GeoMath-2D~3D
  * Author: ARCHISMAN NATH on 5/3/2021 in 10:54 PM
  */
-public class Circle extends UsefulMethods {
+public class Circle  {
 
     public Circle(double radius) {
         UsefulMethods.circleArea(radius);
