6cc187773f443e67e3e18934afe761080dc48761
Archi991-dot <61641517+Archi991-dot@users.noreply.github.com>
2021-05-04 08:48:27 -0400

    added constructor

--- src/Shapes/Square.java
+++ src/Shapes/Square.java
@@ -1,4 +1,11 @@
-package src.Shapes;
+package com.Shapes;
 
-public class Square {
+import com.Functionallity.UsefulMethods;
+
+public class Square extends UsefulMethods {
+
+    public Square(double length, double width, double side) {
+        UsefulMethods.squareArea(length, width);
+        UsefulMethods.squarePerimeter(side);
+    }
 }
