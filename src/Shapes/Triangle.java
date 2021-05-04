e374866e5e3a4f3c62d3fd6459bd342fd7e510fe
Archi991-dot <61641517+Archi991-dot@users.noreply.github.com>
2021-05-04 09:41:55 -0400

    Update Triangle.java

--- src/Shapes/Triangle.java
+++ src/Shapes/Triangle.java
@@ -2,15 +2,18 @@ package com.Shapes;
 
 import com.Functionallity.UsefulMethods;
 
+import javax.swing.*;
+
 /**
  * Automatic Generation Project - Author
  * Project Name: GeoMath-2D~3D
  * Author: ARCHISMAN NATH on 5/3/2021 in 10:54 PM
  */
-public class Square extends UsefulMethods {
+public class Triangle extends JComponent {
 
-    public Square(double length, double width, double side) {
-        UsefulMethods.squareArea(length, width);
-        UsefulMethods.squarePerimeter(side);
+    public Triangle(double base, double height, double side1, double side2, double side3) {
+        UsefulMethods.triangleArea(base, height);
+        UsefulMethods.trianglePerimeter(side1, side2, side3);
+        UsefulMethods.typeOfTriangle(side1, side2, side3);
     }
 }
