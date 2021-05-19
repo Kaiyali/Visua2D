22c5d2b96662af6b3b8b56c57632a51d2647d2d8
Owner <dandankay60@gamil.com>
2021-05-18 21:56:57 -0400

    Panel Color change

---
+++ com/Shapes/Square.java
@@ -0,0 +1,16 @@
+package com.Shapes;
+
+import com.Functionallity.UsefulMethods;
+
+/**
+ * Automatic Generation Project - Author
+ * Project Name: GeoMath-2D~3D
+ * Author: ARCHISMAN NATH on 5/3/2021 in 10:54 PM
+ */
+public class Square {
+
+    public Square(double length, double width, double side) {
+        UsefulMethods.squareArea(length, width);
+        UsefulMethods.squarePerimeter(side);
+    }
+}
