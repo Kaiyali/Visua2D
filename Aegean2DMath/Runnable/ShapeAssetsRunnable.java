de67f87f649b4b001034b183a32ef9370b8e7969
Archisman Nath <61641517+Archi991-dot@users.noreply.github.com>
2021-06-01 12:46:36 -0400

    Added Clear Button Fucntionallity

---
+++ Aegean2DMath/Runnable/ShapeAssetsRunnable.java
@@ -0,0 +1,10 @@
+package Aegean2DMath.Runnable;
+
+import Aegean2DMath.Functionallity.ShapeAssets;
+
+public class ShapeAssetsRunnable {
+
+    public static void main(String[] args) {
+        System.out.println(ShapeAssets.QuadType(1, 1, 23, 23));
+    }
+}
